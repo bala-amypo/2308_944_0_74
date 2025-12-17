9da045616fd5dbdb60dfe4889ac677c3814435c9
bala-amypo <bala@amypo.in>
2025-12-17 08:18:12 +0000

    User commit

---
+++ src/main/java/com/example/demo/newentity/NewFileEntity.java
@@ -0,0 +1,47 @@
+package com.example.demo.newentity;
+
+import java.util.*;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.*;
+
+@Entity
+
+public class NewFileEntity{
+    @Id
+    @GeneratedValue(stratergy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "should not contain spaces")
+    private String name;
+    @NotBlank(message = "should not be empty")
+    @Email(message = "it must be an email")
+    private String email;
+    
+     public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public NewFileEntity(Long id, @NotBlank(message = "should not contain spaces") String name,
+            @NotBlank(message = "should not be empty") @Email(message = "it must be an email") String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+}
